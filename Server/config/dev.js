87ec88480135bd66f6eb43dc7fa7b6f214ea77d8
rashed <rashed0999229437@hotmail.com>
2023-10-30 22:39:37 +0100

    erster Comim

---
+++ Server/config/dev.js
@@ -0,0 +1,13 @@
+module.exports = {
+    mongoURI: 'mongodb://localhost:27017/MRRA',
+    jwtSecret: 'kdjkasjqr8239r8kfj939kdjkdjfkwfdqwfwqfwqfwt944039043kjgkwjgkwgwlkg404204wegklgkt42-owpglw4-y34pyo3py0239tfkeit049igkjkvjvw049t09tofjkwrjt034tigdfkjgkdfjg0rt9304fsdkfj04t904gigo',
+    jwtExpire: '24h',
+    api_key: "",
+    DOMAIN: '',
+    cloudinary_cloud_name: '',
+    cloudinary_api_key: '',
+    cloudinary_api_secret: '',
+    EMAIL: '',
+    PASSWORD: "",
+    stripe_secret: ""
+} 
